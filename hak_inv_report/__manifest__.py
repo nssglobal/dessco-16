fde9ac5c3abfb809780b133361f6a9c8551a3bf0
Hunain AK <55621092+HunainAhmedKhan@users.noreply.github.com>
2022-12-16 13:08:37 +0500

    Add files via upload

--- hak_inv_report/__manifest__.py
+++ hak_inv_report/__manifest__.py
@@ -9,8 +9,8 @@
         Long description of module's purpose
     """,
 
-    'author': "HAKsolutions",
-    'website': "https://www.HAKsolutions.com",
+    'author': "My Company",
+    'website': "https://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
