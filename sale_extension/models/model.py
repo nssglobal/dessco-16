c2bf326fb19d38e08ed0006f996b2e99f271a60a
Hunain AK <55621092+HunainAhmedKhan@users.noreply.github.com>
2022-12-23 17:57:03 +0500

    Update model.py

--- sale_extension/models/model.py
+++ sale_extension/models/model.py
@@ -10,5 +10,15 @@ class SaleOrder(models.Model):
 
     po_ref = fields.Char(string="PO ref")
 
+    
+    
+class ProductTemplateInh(models.Model):
+    _inherit = 'sale.order.line'
+
+    @api.onchange("product_id")
+    def _onchange_product(self):
+        for i in self:
+            i.tax_id=i.product_id.taxes_id
+
 
 
